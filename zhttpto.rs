1ec72daa0e646d8564b95fe795e339d9729e896a
Peter <peter@peter-Lenovo-IdeaPad-Y410P.(none)>
2014-01-28 00:00:49 -0500

    ps1

--- zhttpto.rs
+++ zhttpto.rs
@@ -15,21 +15,37 @@
 use std::io::*;
 use std::io::net::ip::{SocketAddr};
 use std::{str};
+use std::*;
+use std::io::buffered::BufferedReader;
+use std::io::File;
+use std::io::Path;
 
 static IP: &'static str = "127.0.0.1";
 static PORT:        int = 4414;
+static mut VCOUNT: int = 0;
 
 fn main() {
+    //let mut vcount: int = 0;
     let addr = from_str::<SocketAddr>(format!("{:s}:{:d}", IP, PORT)).unwrap();
     let mut acceptor = net::tcp::TcpListener::bind(addr).listen();
     
     println(format!("Listening on [{:s}] ...", addr.to_str()));
     
-    for stream in acceptor.incoming() {
+
+	let mut count = 0;
+    for stream1 in acceptor.incoming() {
+	
         // Spawn a task to handle the connection
-        do spawn {
-            let mut stream = stream;
+	// vcount +=1;
+	let mut viewnum = 0;
+	unsafe {viewnum = addviewer();}
+	viewnum = viewnum/2;
+	println!("visitors: {:d}", viewnum);
+        
+            let mut stream = stream1;
+	 
             
+	    
             match stream {
                 Some(ref mut s) => {
                              match s.peer_name() {
@@ -43,20 +59,63 @@ fn main() {
             let mut buf = [0, ..500];
             stream.read(buf);
             let request_str = str::from_utf8(buf);
-            println(format!("Received request :\n{:s}", request_str));
+		println(format!("Received request :{:s}", request_str));
+		let strget = request_str.slice_to(3);
+		let strlink = request_str.slice_to(14);
+
+		println("HOMELINK:" + strlink);
+		if strlink == "GET / HTTP/1.1"{
+			let numresponse = format!("<h1>Greetings, Peter {:d} </h1>", 12);
+		let response: ~str = 	 ~"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n                  <doctype !html><html><head><title>Hello, Rust!</title>                  <style>body { background-color: #FFF; color: #FFFFFF }                         h1 { font-size:2cm; text-align: center; color: black;}                </style></head>                 <body>                 <h1>Greetings, Krusty!</h1>         "+numresponse+"       </body></html>\r\n";
+			 stream.write(response.as_bytes());
+		}
+		
+					
+
+		else if strget == "GET"{
+			//println("YEAH GET");
+			//println("File: "+ strhttp);
+			let rqlen = request_str.len();
+			
+			
+			println!("Request Len:{:u}", rqlen);
+			let file_name = request_str.slice(5, rqlen - 9);
+			println!("File Len:{:u}", file_name.len());
+			println("Actual File:" + file_name);
+			let practice_file = ~"hello.txt";
+			let path = Path::new(practice_file.clone());
+			let request_file = File::open(&path);
+			//check to see if file
+		 	  
+			let mut reader = BufferedReader::new(request_file);
+		
+			for line in reader.lines() {
+       		        			
+                			//print(line);
+          			  	stream.write(line.as_bytes());
+			
+			}
+	    		
+	  		 
+			
+	
+			}
+			
+			
+		}
+		
+	    
             
-            let response: ~str = 
-                ~"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
-                 <doctype !html><html><head><title>Hello, Rust!</title>
-                 <style>body { background-color: #111; color: #FFEEAA }
-                        h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
-                        h2 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green}
-                 </style></head>
-                 <body>
-                 <h1>Greetings, Krusty!</h1>
-                 </body></html>\r\n";
-            stream.write(response.as_bytes());
-            println!("Connection terminates.");
-        }
-    }
+            
+        
+    
+
 }
+unsafe fn addviewer() ->int{
+	
+	VCOUNT += 1;
+	let num = VCOUNT;
+	//println("visitors: " + int::str(num));
+	return num;
+}
+
